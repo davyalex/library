c7ceb56f73afd3a4b4aabb4f28dcc1fc55be6539
Drico <attanguettiacedric@gmail.com>
2022-05-18 14:41:56 +0000

    register

--- src/views/pages/auth/register.vue
+++ src/views/pages/auth/register.vue
@@ -1,48 +1,28 @@
 <template>
-
   <div class="auth-wrapper auth-v2">
     <b-row class="auth-inner m-0">
-
       <!-- Brand logo-->
       <b-link class="brand-logo">
         <vuexy-logo />
 
-        <h2 class="brand-text text-primary ml-1">
-          Vuexy
-        </h2>
+        <h2 class="brand-text text-primary ml-1">Vuexy</h2>
       </b-link>
       <!-- /Brand logo-->
 
       <!-- Left Text-->
-      <b-col
-        lg="8"
-        class="d-none d-lg-flex align-items-center p-5"
-      >
-        <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
-          <b-img
-            fluid
-            :src="imgUrl"
-            alt="Register V2"
-          />
+      <b-col lg="8" class="d-none d-lg-flex align-items-center p-5">
+        <div
+          class="w-100 d-lg-flex align-items-center justify-content-center px-5"
+        >
+          <b-img fluid :src="imgUrl" alt="Register V2" />
         </div>
       </b-col>
       <!-- /Left Text-->
 
       <!-- Register-->
-      <b-col
-        lg="4"
-        class="d-flex align-items-center auth-bg px-2 p-lg-5"
-      >
-        <b-col
-          sm="8"
-          md="6"
-          lg="12"
-          class="px-xl-2 mx-auto"
-        >
-          <b-card-title
-            title-tag="h2"
-            class="font-weight-bold mb-1"
-          >
+      <b-col lg="4" class="d-flex align-items-center auth-bg px-2 p-lg-5">
+        <b-col sm="8" md="6" lg="12" class="px-xl-2 mx-auto">
+          <b-card-title title-tag="h2" class="font-weight-bold mb-1">
             Adventure starts here 🚀
           </b-card-title>
           <b-card-text class="mb-2">
@@ -52,11 +32,8 @@
           <!-- form -->
           <validation-observer ref="registerForm">
             <b-form class="auth-register-form mt-2">
-               <!-- Nom -->
-              <b-form-group
-                label="Nom"
-                label-for="nom"
-              >
+              <!-- Nom -->
+              <b-form-group label="Nom" label-for="nom">
                 <validation-provider
                   #default="{ errors }"
                   name="nom"
@@ -65,7 +42,7 @@
                   <b-form-input
                     id="nom"
                     v-model="nom"
-                    :state="errors.length > 0 ? false:null"
+                    :state="errors.length > 0 ? false : null"
                     name="nom"
                     placeholder="john"
                   />
@@ -73,12 +50,8 @@
                 </validation-provider>
               </b-form-group>
 
-
               <!-- Prenoms -->
-              <b-form-group
-                label="Prenoms"
-                label-for="prenoms"
-              >
+              <b-form-group label="Prenoms" label-for="prenoms">
                 <validation-provider
                   #default="{ errors }"
                   name="prenoms"
@@ -87,7 +60,7 @@
                   <b-form-input
                     id="prenoms"
                     v-model="prenoms"
-                    :state="errors.length > 0 ? false:null"
+                    :state="errors.length > 0 ? false : null"
                     name="login-email"
                     placeholder="john"
                   />
@@ -95,13 +68,9 @@
                 </validation-provider>
               </b-form-group>
 
-
               <!-- contact -->
 
-               <b-form-group
-                label="Contact"
-                label-for="contact"
-              >
+              <b-form-group label="Contact" label-for="contact">
                 <validation-provider
                   #default="{ errors }"
                   name="contact"
@@ -110,7 +79,7 @@
                   <b-form-input
                     id="contact"
                     v-model="contact"
-                    :state="errors.length > 0 ? false:null"
+                    :state="errors.length > 0 ? false : null"
                     name="contact"
                     placeholder="000 000 000"
                   />
@@ -119,10 +88,7 @@
               </b-form-group>
 
               <!-- email -->
-              <b-form-group
-                label="Email"
-                label-for="register-email"
-              >
+              <b-form-group label="Email" label-for="register-email">
                 <validation-provider
                   #default="{ errors }"
                   name="Email"
@@ -132,7 +98,7 @@
                     id="register-email"
                     v-model="email"
                     name="register-email"
-                    :state="errors.length > 0 ? false:null"
+                    :state="errors.length > 0 ? false : null"
                     placeholder="john@example.com"
                   />
                   <small class="text-danger">{{ errors[0] }}</small>
@@ -140,10 +106,7 @@
               </b-form-group>
 
               <!-- password -->
-              <b-form-group
-                label-for="register-password"
-                label="Password"
-              >
+              <b-form-group label-for="register-password" label="Password">
                 <validation-provider
                   #default="{ errors }"
                   name="Password"
@@ -151,14 +114,14 @@
                 >
                   <b-input-group
                     class="input-group-merge"
-                    :class="errors.length > 0 ? 'is-invalid':null"
+                    :class="errors.length > 0 ? 'is-invalid' : null"
                   >
                     <b-form-input
                       id="register-password"
                       v-model="password"
                       class="form-control-merge"
                       :type="passwordFieldType"
-                      :state="errors.length > 0 ? false:null"
+                      :state="errors.length > 0 ? false : null"
                       name="register-password"
                       placeholder="············"
                     />
@@ -189,7 +152,7 @@
                 variant="primary"
                 block
                 type="submit"
-                @click.prevent="validationForm"
+                @click.prevent="save"
               >
                 Enregistrer
               </b-button>
@@ -198,62 +161,62 @@
 
           <p class="text-center mt-2">
             <span>Already have an account?</span>
-            <b-link :to="{name:'auth-login-v2'}">
+            <b-link :to="{ name: 'auth-login-v2' }">
               <span>&nbsp;Sign in instead</span>
             </b-link>
           </p>
 
           <!-- divider -->
           <div class="divider my-2">
-            <div class="divider-text">
-              or
-            </div>
+            <div class="divider-text">or</div>
           </div>
 
           <div class="auth-footer-btn d-flex justify-content-center">
-            <b-button
-              variant="facebook"
-              href="javascript:void(0)"
-            >
+            <b-button variant="facebook" href="javascript:void(0)">
               <feather-icon icon="FacebookIcon" />
             </b-button>
-            <b-button
-              variant="twitter"
-              href="javascript:void(0)"
-            >
+            <b-button variant="twitter" href="javascript:void(0)">
               <feather-icon icon="TwitterIcon" />
             </b-button>
-            <b-button
-              variant="google"
-              href="javascript:void(0)"
-            >
+            <b-button variant="google" href="javascript:void(0)">
               <feather-icon icon="MailIcon" />
             </b-button>
-            <b-button
-              variant="github"
-              href="javascript:void(0)"
-            >
+            <b-button variant="github" href="javascript:void(0)">
               <feather-icon icon="GithubIcon" />
             </b-button>
           </div>
         </b-col>
       </b-col>
-    <!-- /Register-->
+      <!-- /Register-->
     </b-row>
   </div>
 </template>
 
 <script>
 /* eslint-disable global-require */
-import { ValidationProvider, ValidationObserver } from 'vee-validate'
-import VuexyLogo from '@core/layouts/components/Logo.vue'
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import VuexyLogo from "@core/layouts/components/Logo.vue";
 import {
-  BRow, BCol, BLink, BButton, BForm, BFormCheckbox, BFormGroup, BFormInput, BInputGroup, BInputGroupAppend, BImg, BCardTitle, BCardText,
-} from 'bootstrap-vue'
-import { required, email } from '@validations'
-import { togglePasswordVisibility } from '@core/mixins/ui/forms'
-import store from '@/store/index'
-import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
+  BRow,
+  BCol,
+  BLink,
+  BButton,
+  BForm,
+  BFormCheckbox,
+  BFormGroup,
+  BFormInput,
+  BInputGroup,
+  BInputGroupAppend,
+  BImg,
+  BCardTitle,
+  BCardText,
+} from "bootstrap-vue";
+import { required, email } from "@validations";
+import { togglePasswordVisibility } from "@core/mixins/ui/forms";
+import store from "@/store/index";
+import axios from "axios";
+import URL, { APP_HOST } from '@/views/pages/request';
+import ToastificationContent from "@core/components/toastification/ToastificationContent.vue";
 
 export default {
   components: {
@@ -278,53 +241,77 @@ export default {
   mixins: [togglePasswordVisibility],
   data() {
     return {
-      status: '',
-      nom: '',
-      prenoms: '',
-      email: '',
-      password: '',
-      sideImg: require('@/assets/images/pages/register-v2.svg'),
+      status: "",
+      nom: "",
+      prenoms: "",
+      email: "",
+      contact: "",
+      password: "",
+      sideImg: require("@/assets/images/pages/register-v2.svg"),
       // validation
       required,
       email,
-    }
+    };
   },
   computed: {
     passwordToggleIcon() {
-      return this.passwordFieldType === 'password' ? 'EyeIcon' : 'EyeOffIcon'
+      return this.passwordFieldType === "password" ? "EyeIcon" : "EyeOffIcon";
     },
     imgUrl() {
-      if (store.state.appConfig.layout.skin === 'dark') {
+      if (store.state.appConfig.layout.skin === "dark") {
         // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-        this.sideImg = require('@/assets/images/pages/register-v2-dark.svg')
-        return this.sideImg
+        this.sideImg = require("@/assets/images/pages/register-v2-dark.svg");
+        return this.sideImg;
       }
-      return this.sideImg
+      return this.sideImg;
     },
   },
   methods: {
     validationForm() {
-      this.$refs.registerForm.validate().then(success => {
+      this.$refs.registerForm.validate().then((success) => {
         if (success) {
           this.$toast({
             component: ToastificationContent,
             props: {
-              title: 'Form Submitted',
-              icon: 'EditIcon',
-              variant: 'success',
+              title: "Form Submitted",
+              icon: "EditIcon",
+              variant: "success",
             },
-          })
+          });
         }
-      })
+      });
     },
-    
 
-    
+    async save() {
+      try {
+
+         const config = {
+          headers: {
+            Accept: 'application/json',
+          },
+        };
+
+        const data = {
+          nom: this.nom,
+          prenoms: this.prenoms,
+          email: this.email,
+          contact: this.contact,
+          password: this.password,
+        };
+        await axios.post(URL.REGISTER, data, config).then((response) => {
+          this.userData = response.data;
+          cosole.log(this.userData);
+        });
+      } catch (error) {
+        this.marche = false;
+        console.log(error);
+      }
+    },
   },
-}
+};
 /* eslint-disable global-require */
 </script>
 
 <style lang="scss">
-@import '@core/scss/vue/pages/page-auth.scss';
+@import "@core/scss/vue/pages/page-auth.scss";
 </style>
